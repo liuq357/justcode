7b3045bf742e481d8ad9aca4703f75f8495c92be
Qiang Liu <liuq357@gmail.com>
2023-09-19 00:41:22 +0800

    set up blind problem

--- main.py
+++ main.py
@@ -14,6 +14,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     so = blind.Solution()
-    s = " "
-    nums = [-2,1,-3,4,-1,2,1,-5,4]
-    print("result", so.maxSubArray(nums))
+    nums = [3,2,1,0,4]
+    print("result", so.canJump(nums))
--- neetcode/blind.py
+++ neetcode/blind.py
@@ -20,3 +20,38 @@ class Solution:
             m_sum = c_sum
 
         return m_sum
+
+    # Sum of two integer
+    def getSum(self, a: int, b: int) -> int:
+        s = a ^ b
+        c = a & b
+        while c != 0:
+            c = c << 1
+            new_s = s ^ c
+            c = s & c
+            s = new_s
+
+        return s
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for i in range(len(text2) + 1)] for j in range(len(text1) + 1)]
+        for i in range(1, len(text1) + 1):
+            for j in range(1, len(text2) + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[len(text1)][len(text2)]
+
+    def canJump(self, nums: List[int]) -> bool:
+        m = nums[0]
+        for i in range(0, len(nums)):
+            if i > m:
+                break
+
+            m = max(m, nums[i] + i)
+            if m >= len(nums) - 1:
+                return True
+
+        return False
